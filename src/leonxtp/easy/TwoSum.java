a29eb83a8ff8f0e35819f60118771dc8e00ac50d
LeonXtp <1965976573@qq.com>
2017-10-08 11:16:51 +0800

    first submit solution

---
+++ src/leonxtp/easy/TwoSum.java
@@ -0,0 +1,25 @@
+package leonxtp.easy;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String args[]) {
+        TwoSum twoSum = new TwoSum();
+        int[] sample = {2, 7, 11, 15, 33, 49, 50};
+        int[] result = twoSum.twoSum(sample, 99);
+        if (result != null) {
+            System.out.println(result[0] + ", " + result[1]);
+        } else {
+            System.out.println("Solution doesn't exist!");
+        }
+    }
+}
