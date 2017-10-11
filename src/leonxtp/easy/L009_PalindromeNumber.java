6a2ff7ce1bfb803f91ecdec0b7d76cd5b72305f8
LeonXtp <1965976573@qq.com>
2017-10-11 10:07:05 +0800

    PalindromeNumber

--- src/leonxtp/easy/L009_PalindromeNumber.java
+++ src/leonxtp/easy/L009_PalindromeNumber.java
@@ -7,7 +7,46 @@ package leonxtp.easy;
  */
 public class L009_PalindromeNumber {
 
+    public static void main(String args[]) {
+        L009_PalindromeNumber test = new L009_PalindromeNumber();
+        System.out.println(test.isPalindrome(22));
+    }
+
     public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        if (x < 10 || x % 11 == 0 && x < 100) {
+            return true;
+        }
+
+        int bigDivision = 1000000000;
+        int smallMod = 10;
+
+        int bigTemp = 0, small;
+
+        while (x / bigDivision == 0) {
+            bigDivision /= 10;
+        }
+
+        // 1223221
+        while (bigDivision >= smallMod) {
+            if (x >= 1000000000 && bigDivision == 1000000000) {
+                bigTemp = x / bigDivision;
+            } else {
+                bigTemp = (x % (bigDivision * 10)) / bigDivision;
+            }
+            small = (x % smallMod) / (smallMod / 10);
+            if (bigTemp == small) {
+                bigDivision /= 10;
+                smallMod *= 10;
+            } else {
+                return false;
+            }
+        }
+
         return true;
     }
 
