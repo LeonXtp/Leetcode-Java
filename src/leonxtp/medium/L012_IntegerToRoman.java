85e994cc4fecdd60a932054fb2c8871a06f6da1f
LeonXtp <1965976573@qq.com>
2017-10-15 23:18:14 +0800

    Romans

---
+++ src/leonxtp/medium/L012_IntegerToRoman.java
@@ -0,0 +1,27 @@
+package leonxtp.medium;
+
+/**
+ *
+ */
+public class L012_IntegerToRoman {
+
+    private static final String[] ROMANS = {"I", "V", "X", "L", "C", "D", "M"};
+
+    public static void main(String args[]) {
+
+    }
+
+    public String intToRoman(int num) {
+
+
+        StringBuffer sb = new StringBuffer();
+
+
+        return "";
+    }
+
+    private int getRomanNumber(int number){
+        return 0;
+    }
+
+}
