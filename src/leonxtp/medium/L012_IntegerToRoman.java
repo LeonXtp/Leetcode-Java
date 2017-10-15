fb236aaf0af0294b1521d4d18626fd02ae7c73bd
LeonXtp <1965976573@qq.com>
2017-10-15 23:48:57 +0800

    IntegerToRoman

--- src/leonxtp/medium/L012_IntegerToRoman.java
+++ src/leonxtp/medium/L012_IntegerToRoman.java
@@ -20,8 +20,26 @@ public class L012_IntegerToRoman {
         return "";
     }
 
-    private int getRomanNumber(int number){
-        return 0;
+    private String getRomanNumber(int number) {
+        switch (number) {
+            case 1:
+                return ROMANS[0];
+            case 5:
+                return ROMANS[1];
+            case 10:
+                return ROMANS[2];
+            case 50:
+                return ROMANS[3];
+            case 100:
+                return ROMANS[4];
+            case 500:
+                return ROMANS[5];
+            case 1000:
+                return ROMANS[6];
+            default:
+                break;
+        }
+        return "";
     }
 
 }
