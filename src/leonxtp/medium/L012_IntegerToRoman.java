96791efd8c438cadece8a11bfabedb2d4d16b612
LeonXtp <1965976573@qq.com>
2017-10-19 23:51:11 +0800

    int2roman not ready

--- src/leonxtp/medium/L012_IntegerToRoman.java
+++ src/leonxtp/medium/L012_IntegerToRoman.java
@@ -1,11 +1,13 @@
 package leonxtp.medium;
 
 /**
- *
+ * 1、3、5、7、9
+ * 13、17
  */
 public class L012_IntegerToRoman {
 
-    private static final String[] ROMANS = {"I", "V", "X", "L", "C", "D", "M"};
+    private static final String[] ROMANS = {"I", "II", "III", "IV", "V", "VI", "VII"
+    ,"VIII", "IX", "X"};
 
     public static void main(String args[]) {
 
@@ -17,31 +19,44 @@ public class L012_IntegerToRoman {
         StringBuffer sb = new StringBuffer();
 
         int mod = 10;
-        
+        while (num / mod > 0) {
+            int bitNum = (num % mod) / (mod > 10 ? mod : 1);
+            if (bitNum <= 3) {
+
+            } else {
+
+            }
+            sb.insert(0, )
+            mod *= 10;
+        }
 
 
         return "";
     }
 
+    //1437  MCDXXXVII
     private String getRomanNumber(int number) {
-        switch (number) {
-            case 1:
-                return ROMANS[0];
-            case 5:
-                return ROMANS[1];
-            case 10:
-                return ROMANS[2];
-            case 50:
-                return ROMANS[3];
-            case 100:
-                return ROMANS[4];
-            case 500:
-                return ROMANS[5];
-            case 1000:
-                return ROMANS[6];
-            default:
-                break;
+        if (number < 10) {
+            switch (number) {
+                case 1:
+                    return ROMANS[0];
+                case 2:
+                    return ROMANS[1];
+                case 3:
+                    return ROMANS[2];
+                case 4:
+                    return ROMANS[3];
+                case 5:
+                    return ROMANS[4];
+                case 6:
+                    return ROMANS[5];
+                case 7:
+                    return ROMANS[6];
+                default:
+                    break;
+            }
         }
+
         return "";
     }
 
