b158a55d06ef730bd2a7b873c62cb6523db2fe12
LeonXtp <1965976573@qq.com>
2017-10-20 00:57:18 +0800

    if-else < StringBuffer < '+' < StringBuilder

--- src/leonxtp/medium/L012_IntegerToRoman.java
+++ src/leonxtp/medium/L012_IntegerToRoman.java
@@ -1,76 +1,40 @@
 package leonxtp.medium;
 
-import sun.jvm.hotspot.debugger.win32.coff.COFFFile;
-
 /**
- * 1、3、5、7、9
- * 13、17
+ * I,V,X,
+ * L,C,D,M
  */
 public class L012_IntegerToRoman {
 
-    private static final String[] ROMANS_1 = {"I", "II", "III", "IV", "V", "VI", "VII"
-            , "VIII", "IX"};
-
-    private static final String[] ROMANS_10 = {"I", "II", "III", "IV", "V", "VI", "VII"
-            , "VIII", "IX"};
-
-    private static final String[] ROMANS_100 = {"I", "II", "III", "IV", "V", "VI", "VII"
-            , "VIII", "IX"};
-
-    private static final String[] ROMANS_1000 = {"I", "II", "III", "IV", "V", "VI", "VII"
-            , "VIII", "IX"};
-
     public static void main(String args[]) {
+        L012_IntegerToRoman test = new L012_IntegerToRoman();
+        int[] tests = {1, 3, 5, 7, 9, 13, 15, 17, 19, 20, 30, 40, 50, 60, 90, 100,
+                111, 200, 400, 900, 1437, 3333};
 
-    }
-
-    public String intToRoman(int num) {
-
-
-        StringBuffer sb = new StringBuffer();
-
-        int mod = 10;
-        while (num / mod > 0) {
-            int bitNum = (num % mod) / (mod > 10 ? mod : 1);
-            if (bitNum <= 3) {
-
-            } else {
-
-            }
-            sb.insert(0, )
-            mod *= 10;
+        for (int data : tests) {
+            System.out.println(test.intToRoman2(data));
         }
 
-
-        return "";
     }
 
-    //1437  MCDXXXVII
-    private String getRomanNumber(int number, int coefficient) {
+    public String intToRoman2(int num) {
+
+        String[] romans1 = {"", "I", "II", "III", "IV", "V", "VI", "VII"
+                , "VIII", "IX"};
+
+        String[] romans10 = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX"
+                , "LXXX", "XC"};
+
+        String[] romans100 = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC"
+                , "DCCC", "CM"};
+
+        String[] romans1000 = {"", "M", "MM", "MMM"};
 
         StringBuilder sb = new StringBuilder();
-        switch (coefficient) {
-            case 1:
-                if (number <= 3) {
-                    while (number > 0) {
-                        sb.append("I");
-                        number--;
-                    }
-                } else if (3 < number && number <= 8) {
-
-                } else {
-
-                }
-                break;
-            case 10:
-                break;
-            case 100:
-                break;
-            case 1000:
-                break;
-        }
-
-        return "";
+        return sb.append(romans1000[(num % 10000) / 1000])
+                .append(romans100[(num % 1000) / 100])
+                .append(romans10[(num % 100) / 10])
+                .append(romans1[(num % 10)]).toString();
     }
 
 }
