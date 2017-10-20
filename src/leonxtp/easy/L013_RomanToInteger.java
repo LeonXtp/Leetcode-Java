1359b8f6d996656765b314850201b905d208da31
LeonXtp <1965976573@qq.com>
2017-10-20 09:24:36 +0800

    Roman to Integer optimize

--- src/leonxtp/easy/L013_RomanToInteger.java
+++ src/leonxtp/easy/L013_RomanToInteger.java
@@ -4,7 +4,44 @@ public class L013_RomanToInteger {
 
     public static void main(String[] args) {
         L013_RomanToInteger test = new L013_RomanToInteger();
-        System.out.println(test.romanToInt("XX"));
+        System.out.println(test.romanToInt2("MDCCCLXXXIV"));
+    }
+
+    /**
+     * 思路：
+     * 只有I,X,C三个数字是可以在其他数字左边做减法的
+     */
+    public int romanToInt2(String s) {
+        int result = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            switch (ch) {
+                case 'I':
+                    result += result >= 5 ? -1 : 1;
+                    break;
+                case 'V':
+                    result += 5;
+                    break;
+                case 'X':
+                    result += result >= 50 ? -10 : 10;
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'C':
+                    result += result >= 500 ? -100 : 100;
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'M':
+                    result += 1000;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
     }
 
 
